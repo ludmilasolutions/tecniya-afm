e7877f6e00e96ea94466c8afbfce4b8ff67dfdcb
ludmilasolutions <ludmilasolutions2.0@gmail.com>
2026-03-10 11:05:17 -0300

    Add files via upload

--- js/pwa.js
+++ js/pwa.js
@@ -62,18 +62,7 @@ export function generateManifest() {
     display: "standalone",
     background_color: "#020617",
     theme_color: "#020617",
-    icons: [
-      {
-        src: "assets/icon-192.png",
-        sizes: "192x192",
-        type: "image/png"
-      },
-      {
-        src: "assets/icon-512.png",
-        sizes: "512x512",
-        type: "image/png"
-      }
-    ]
+    icons: []
   };
   
   const manifestJson = JSON.stringify(manifest, null, 2);
